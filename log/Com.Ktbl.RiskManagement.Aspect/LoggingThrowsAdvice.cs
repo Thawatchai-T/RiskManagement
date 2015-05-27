a04604689626cd45cc2a3778a73ef63acbbc609c
Woody <m_oji@live.com>
2015-05-27 16:15:51 +0700

    init Log
    Signed-off-by: Woody <m_oji@live.com>

---
+++ log/Com.Ktbl.RiskManagement.Aspect/LoggingThrowsAdvice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spring.Aop;
+using System.Reflection;
+using System.Data.SqlClient;
+using System.Runtime.Remoting;
+
+namespace Com.Ktbl.RiskManagement.Aspect
+{
+    public class LoggingThrowsAdvice: IThrowsAdvice
+    {
+        public string MongoDBConnection { get; set; }
+
+        public void AfterThrowing(Exception ex)
+        {
+            Console.Error.WriteLine(
+                String.Format("Advised method threw this exception : {0}", ex.Message));
+        }
+
+        public void AfterThrowing(MethodInfo method, object[] args, object target, SqlException ex)
+        {
+            //Do something will all arguments
+        }
+
+        public void AfterThrowing(RemotingException ex)
+        {
+            // Do something with remoting exception
+        }
+    }
+}
---
+++ log/Com.Ktbl.RiskManagement.MongoDB/Log/MongoAppenderField.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net.Layout;
+
+namespace Com.Ktbl.RiskManagement.MongoDB.Log
+{
+    public class MongoAppenderField
+    {
+        /// <summary>
+        /// Gets or sets the name of the log field
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the log layout type that will format the final log entry
+        /// </summary>
+        public IRawLayout Layout { get; set; }
+
+        /// <summary>
+        /// Gets or sets the log format value
+        /// </summary>
+        public String Value { get; set; }
+    }
+}
